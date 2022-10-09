319fb64bf7fde300bdeb7b70ddb2e8fd7f73fadc
rakuiit <en.rakeshsharma@gmail.com>
2022-10-09 22:17:30 +0530

    web app ready

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import json
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
@@ -26,6 +27,19 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)
+    return render_template("home.html",prediction_text="The house price prediction is {}".format(output))
+
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True) 
